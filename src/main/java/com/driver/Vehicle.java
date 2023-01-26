eed282550fe2c32c69a2caf6ef85bf8bb6f90fa0
itsviveksinghania <vickykumarsify@gmail.com>
2023-01-26 20:41:58 +0530

    solution2

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,6 +6,23 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+
     public int getCurrentDirection() {
         return currentDirection;
     }
